49d888d0384db1ccd01b2c35a77a6bcf0f994409
Theresa Swayne <theresaswayne@users.noreply.github.com>
2025-12-10 14:54:17 -0500

    Update summarize_roi_measurements.R
    efforts to plot histogram with %

--- summarize_roi_measurements.R
+++ summarize_roi_measurements.R
@@ -24,15 +24,25 @@ summary(df$Ratio_CytoConstrToCell_Corr_Mean) # for a specific column
 
 # ---- Plot important variables ----
 
+bins <- seq (0,1,by=0.05)
+
 # for new data
-hist(No36_1202$Ratio_CytoConstrToNuc_Corr_Mean, xlim = c(0,1), labels = FALSE)
-hist(No36_1202$Ratio_CytoToNuc_Corr_Mean, xlim = c(0,1))
+hist(No36_1202$Ratio_CytoConstrToNuc_Corr_Mean, xlim = c(0,1), breaks = bins, labels = FALSE)
+hist(No36_1202$Ratio_CytoToNuc_Corr_Mean, breaks = bins, xlim = c(0,1))
 
 # for old data
-hist(No36_1118$Ratio_CytoNuc_Corr_Mean, xlim = c(0,1))
+hist(No36_1118$Ratio_CytoNuc_Corr_Mean, breaks = 20, xlim = c(0,1))
 
-p <- ggplot(data = df, aes(x = Ratio_CytoConstrToNuc_Corr_Mean)) +
+p <- ggplot(data = No6, aes(x = Ratio_CytoConstrToNuc_Corr_Mean)) +
   geom_histogram() +
   xlim = c(0,1)
-ggsave("foo.png")
+
+plt <- ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, y = after_stat(prop), group = 1))
+
+plt <- ggplot(data = No6) +
+  geom_bar(mapping = aes(x = Ratio_CytoConstrToNuc_Corr_Mean, 
+                         y = after_stat(prop))) +
+
+#ggsave("foo.png")
 p
